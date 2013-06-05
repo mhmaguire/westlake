e6e7402d993d47d01ef6a655d95d587806ca34f9
Matthew Maguire <mhmaguire0@gmail.com>
2013-06-05 00:24:58 -0700

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,26 @@ namespace :deploy do
 	end
 end
 after "bundle:install", "deploy:symlink_database_yml"
+
+
+namespace :unicorn do
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, except: { no_release: true } do
+    run "kill -s USR2 `cat /tmp/unicorn.WestlakePro.pid`"
+  end
+
+  desc "Start unicorn"
+  task :start, except: { no_release: true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, except: { no_release: true } do
+    run "kill -s QUIT `cat /tmp/unicorn.WestlakePro.pid`"
+  end
+end
+
+after "deploy:restart", "unicorn:restart"
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
