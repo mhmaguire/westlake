a4d5e62ba032408db441d93f717fdf674d03c049
Matthew Maguire <mhmaguire0@gmail.com>
2013-06-04 21:40:02 -0700

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,22 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "WestlakePro"
+set :repository,  "git@github.com:mhmaguire/westlake.git"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+server = "198.61.201.4"
+role :web, server                          # Your HTTP server, Apache/etc
+role :app, server                          # This may be the same as your `Web` server
+role :db,  server, :primary => true # This is where Rails migrations will run
 
+set :user, "westlake"
+set :deploy_to, "/home/westlake/#{application}"
+
+set :user_sudo, false 
+
+default_run_options[:shell]= '/bin/bash --login'
+
+default_environment["RAILS_ENV"] = 'production'
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
