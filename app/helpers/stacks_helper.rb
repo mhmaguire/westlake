18cd7f3dfdfc0ecfd6bb5a868604ee62ee5545b5
Matthew Maguire <mhmaguire0@gmail.com>
2013-06-12 11:25:34 -0700

    fixed layouts

--- app/helpers/stacks_helper.rb
+++ app/helpers/stacks_helper.rb
@@ -1,5 +1,5 @@
 module StacksHelper
 	def get_template(slide)
-		slide_template = "#{slide.css_class}"||'slide_layout' 
+		slide_template = slide.css_class.nil? ? 'slide_layout' : slide.css_class  
 	end
 end
