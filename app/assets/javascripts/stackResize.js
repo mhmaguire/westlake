4cac7dbabacf10cfff51bab12898320276a21cf8
Matthew Maguire <mhmaguire0@gmail.com>
2013-06-27 20:04:42 -0700

    prevented js errors

--- app/assets/javascripts/stackResize.js
+++ app/assets/javascripts/stackResize.js
@@ -2,14 +2,16 @@ if (window.matchMedia("(min-device-width: 768px)").matches) {
     $(window).resize(function (){
             var sSCopy = $('.defaultSlide2 .stackSlideCopy');
             var sSImg = $('.defaultSlide2 .slideContent > img');
-            var footP = $('#mainFoot').offset().top;
-            var copyP = $(sSImg).offset().top + sSImg.height() + $(sSCopy).height();
+            if (sSImg.length) {
+                var footP = $('#mainFoot').offset().top;
+                var copyP = $(sSImg).offset().top + sSImg.height() + $(sSCopy).height();
 
-            if (footP < copyP ){
-                sSCopy.css({'position':'fixed','bottom':'40px'});
-            }
-            else{
-                sSCopy.css('position', 'static');
+                if (footP < copyP ){
+                    sSCopy.css({'position':'fixed','bottom':'50px'});
+                }
+                else{
+                    sSCopy.css('position', 'static');
+                }
             }
         });
 }
