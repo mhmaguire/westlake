65f3d3c04e98a79ccd3ea3d126d0de1a5a6a1ced
Matthew Maguire <mhmaguire0@gmail.com>
2013-08-14 12:47:42 -0700

    changed galleryimage attachment size

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -2,7 +2,7 @@ class GalleryImage < ActiveRecord::Base
   before_save :set_content_association
   attr_accessible :image, :gallery_type, :caption
   retina!
-  has_attached_file :image, styles: {small: "150x150>", large: "600x600>"}, retina: true
+  has_attached_file :image, styles: {small: "150x150>", large: "1400x1400>"}, retina: true
 
   belongs_to :content
 
